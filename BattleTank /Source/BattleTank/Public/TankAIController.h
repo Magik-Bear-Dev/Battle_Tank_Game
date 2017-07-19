c0064a530bb7e0afedbde76d12311288bec0fad0
marksappstudio <marksappstudio>
2017-07-19 16:36:59 +1000

    S4, Lecture 125 - Get the Player Controller with C++

--- BattleTank /Source/BattleTank/Public/TankAIController.h
+++ BattleTank /Source/BattleTank/Public/TankAIController.h
@@ -3,20 +3,21 @@
 #pragma once
 
 #include "Tank.h"
-#include "CoreMinimal.h"
 #include "AIController.h"
 #include "TankAIController.generated.h"
 
 /**
- * 
+ *
  */
 UCLASS()
 class BATTLETANK_API ATankAIController : public AAIController
 {
-	GENERATED_BODY()
+    GENERATED_BODY()
     
+private:
     virtual void BeginPlay() override;
     
     ATank* GetControlledTank() const;
     
+    ATank* GetPlayerTank() const;
 };
