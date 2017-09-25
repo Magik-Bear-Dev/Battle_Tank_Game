4bb06a08c7f168d608b92a3d8545dcce2271fc75
marksappstudio <marksappstudio>
2017-09-25 17:14:57 +1000

    S4, Lecture 141 - Review Our Execution Flow

--- BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
@@ -12,7 +12,7 @@ UTankAimingComponent::UTankAimingComponent()
     // Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
     // off to improve performance if you don't need them.
     bWantsBeginPlay = true;
-    PrimaryComponentTick.bCanEverTick = true;
+    PrimaryComponentTick.bCanEverTick = true; // TODO Should this really ticK?
     
     // ...
 }
@@ -41,6 +41,13 @@ void UTankAimingComponent::AimAt(FVector HitLocation, float LaunchSpeed)
     {
         auto AimDirection = OutLaunchVelocity.GetSafeNormal();
         MoveBarrelTowards(AimDirection);
+        auto Time = GetWorld()->GetTimeSeconds();
+        UE_LOG(LogTemp, Warning, TEXT("%f: aim solution found"), Time);
+    }
+    else
+    {
+        auto Time = GetWorld()->GetTimeSeconds();
+        UE_LOG(LogTemp, Warning, TEXT("%f: no aim solve found"), Time);
     }
     // If no solution found do nothing
 }
