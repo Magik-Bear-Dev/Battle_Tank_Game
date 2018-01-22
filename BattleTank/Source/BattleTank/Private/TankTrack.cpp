689fa120839da2685906cafa94d11f2b78438582
marksappstudio <marksappstudio>
2018-01-22 15:48:12 +1100

    S4, Lecture 184 - Avoiding Boolean Flags

--- BattleTank/Source/BattleTank/Private/TankTrack.cpp
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -4,7 +4,7 @@
 
 UTankTrack::UTankTrack()
 {
-    PrimaryComponentTick.bCanEverTick = true;
+    PrimaryComponentTick.bCanEverTick = false;
 }
 
 void UTankTrack::BeginPlay()
@@ -14,26 +14,35 @@ void UTankTrack::BeginPlay()
 
 void UTankTrack::OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit)
 {
-    UE_LOG(LogTemp, Warning, TEXT("I'm hit, I'm hit!"))
+    DriveTrack();
+    ApplySidewaysForce();
+    CurrentThrottle = 0;
 }
 
-void UTankTrack::TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction)
+void UTankTrack::ApplySidewaysForce()
 {
+    // Work-out the required acceleration this frame to correct
     auto SlippageSpeed = FVector::DotProduct(GetRightVector(), GetComponentVelocity());
-    
-  // Work-out requeired acceleration this from to correct
+    auto DeltaTime = GetWorld()->GetDeltaSeconds();
     auto CorrectionAcceleration = -SlippageSpeed / DeltaTime * GetRightVector();
-  // Calculate and apply sideways (F = m a )
+    
+    // Calculate and apply sideways (F = m a)
     auto TankRoot = Cast<UStaticMeshComponent>(GetOwner()->GetRootComponent());
     auto CorrectionForce = (TankRoot->GetMass() * CorrectionAcceleration) / 2; // Two tracks
     TankRoot->AddForce(CorrectionForce);
 }
 
-
 void UTankTrack::SetThrottle(float Throttle)
 {
-    auto ForceApplied = GetForwardVector() * Throttle * TrackMaxDrivingForce;
+    CurrentThrottle = FMath::Clamp<float>(CurrentThrottle + Throttle, -1, 1);
+}
+
+void UTankTrack::DriveTrack()
+{
+    auto ForceApplied = GetForwardVector() * CurrentThrottle * TrackMaxDrivingForce;
     auto ForceLocation = GetComponentLocation();
     auto TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
     TankRoot->AddForceAtLocation(ForceApplied, ForceLocation);
 }
+
+
