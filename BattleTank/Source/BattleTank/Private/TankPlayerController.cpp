f1899d7fa134db1d6496df6bff74d2e221454a16
marksappstudio <marksappstudio>
2018-01-30 17:15:05 +1100

    S4, Lecture 185 - Improving Tank Aiming

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -30,7 +30,8 @@ void ATankPlayerController::AimTowardsCrosshair()
     if (!AimingComponent) { return; }
     
     FVector HitLocation; // Out parameter
-    if (GetSightRayHitLocation(HitLocation)) // Has "side-effect", is going to line trace
+    bool bGotHitLocation = GetSightRayHitLocation(HitLocation);
+    if (bGotHitLocation) // Has "side-effect", is going to line trace
     {
         AimingComponent->AimAt(HitLocation);
     }
@@ -49,11 +50,9 @@ bool ATankPlayerController::GetSightRayHitLocation(FVector& HitLocation) const
     if (GetLookDirection(ScreenLocation, LookDirection))
     {
         // Line-trace along that LookDrection, and see what we hit (up to max range)
-        GetVectorHitLocation(LookDirection, HitLocation);
+       return GetVectorHitLocation(LookDirection, HitLocation);
     }
-    
-    
-    return true;
+    return false;
 }
 
 bool ATankPlayerController::GetVectorHitLocation(FVector LookDirection, FVector& HitLocation) const
